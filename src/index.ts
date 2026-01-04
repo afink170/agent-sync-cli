ef68c52fde65c02c1cfb646831e98db2d5f933f5
afink170 <afink170@yahoo.com>
2026-01-04 18:06:09 +0000

    feat: add --version option

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { logger } from '@/core/logger';
 import { createCli } from 'trpc-cli';
+import { version } from '../package.json';
 import { router } from './router';
 
-createCli({ router, name: 'agent-sync' }).run({ logger });
+createCli({ router, name: 'agent-sync', version }).run({ logger });
